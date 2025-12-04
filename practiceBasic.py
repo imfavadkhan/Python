ff0ac1b0933f6c13eb0ecfe40d57db50a4c53507
Fawad Khan <imfavadkhan@gmail.com>
2025-12-04 21:31:58 +0500

    Add files via upload

---
+++ practiceBasic.py
@@ -0,0 +1,27 @@
+a = 234E-23
+b = 20E5
+print(a)
+print(b)
+c = complex(4 , 6.3)
+print(c)
+d = complex(6)
+print(d)
+print("Number systems in pyhton:-")
+a1 = 10
+a2 = 0b1010
+a3 = 0o12
+a4 = 0xA
+print(f"10 in Binary is {a1}")
+print(f"10 in binary is {a2}")
+print(f"10 in OCtal is {a3}")
+print(f"10 in Hexadecimal is {a4}")
+z = 10 
+print("Binary of z :",bin(z))
+print("Octal of z is : ",oct(z))
+print("Hexadecimal of z is :",hex(z))
+print(type(z))
+print(type(bin(z)))
+x = 'abcd'
+y = 'efgh'
+# print(x-y)
+print(8 and 4)
